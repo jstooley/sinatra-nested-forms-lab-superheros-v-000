2b60ba637ac7ec99ad95436ff5021cf2e9eec567
Justin Tooley <jstooley823@gmail.com>
2017-11-13 20:53:34 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,9 +1,10 @@
 class SuperHero
   attr_reader :name, :power, :bio
-
+  @@all = []
   def initialize(hash)
     @name = hash[:name]
     @power = hash[:power]
     @bio = hash[:bio]
   end
+  
 end
